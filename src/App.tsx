ae9d595c3eee17318b8cf6254f45c3e121ad8487
Merih <rya89128@gmail.com>
2026-03-20 14:29:07 +0900

    Use inline GA snippet

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,5 @@
-import { useEffect } from 'react';
 import { MnemosyneTerminal } from './lore/MnemosyneTerminal';
-import { initializeAnalytics } from './utils/analytics';
 
 export default function App() {
-  useEffect(() => {
-    initializeAnalytics();
-  }, []);
-
   return <MnemosyneTerminal />;
 }
